83bdab0d4fbfa0a99d844ae0c3c6a013bf0760b6
Willian Ferreira <willianferreira@Mac-Willian.local>
2022-01-29 14:08:54 -0300

    Translating print texts into English

--- musical_scale.py
+++ musical_scale.py
@@ -131,25 +131,25 @@ def main():
     try:
         args = sys.argv[1:]
         if not args:
-            print("Você deve informar a nota a ser gerada a escala. Utilize o --help ou -h para maiores informações.")
+            print("You must inform the note to be generated in the scale.\n Use --help or -h for more information.")
             sys.exit(1)
 
         if args[0] in ["--natural_minor"] and  args[1] in ["C", "D", "E", "F", "G", "A", "B"]:
-            print("Escala de {} Menor".format(args[1]))
+            print("{} minor scale".format(args[1]))
             print(generateScale(args[1], args[0]))
             sys.exit(1)
         
         elif args[0] in ["--major"] and args[1] in ["C", "D", "E", "F", "G", "A", "B"]:
-            print("Escala de {} Maior".format(args[1]))
+            print("{} major scale".format(args[1]))
             print(generateScale(args[1], args[0]))
             sys.exit(1)
         elif args[0] in ["--help", "-h"]:
             help()
         else:
-            print("Informação inválida. Utilize o --help ou -h para maiores informações.")
+            print("Invalid information. Use --help or -h for more information.")
             sys.exit(1)
     except Exception as e:
-        print("Erro inesperado identificado. Favor contatar o admonistrador. Erro: {}".format(e))
+        print("Unexpected error identified. Please contact the administrator. Error: {}".format(e))
         sys.exit(1)
     
 if __name__ == '__main__':
